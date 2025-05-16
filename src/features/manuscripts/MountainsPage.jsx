487edc931e93c321f3703165215e4c8429924e6e
hollowlamp <2504384479@qq.com>
2025-05-17 03:43:20 +0800

    feat: 使用antd组件搭建dashboard示例框架

---
+++ src/features/manuscripts/MountainsPage.jsx
@@ -0,0 +1,3 @@
+export const Component = () => {
+  return <div>山地公苑</div>;
+};
--- src/router/routes.js
+++ src/router/routes.js
@@ -6,6 +6,37 @@ export const routes = [
   {
     path: '/',
     lazy: () => import('../features/dashboard/DashboardPage'),
+    children: [
+      {
+        path: 'manuscripts',
+        children: [
+          {
+            path: 'forest',
+            lazy: () => import('../features/manuscripts/ForestPage'),
+          },
+          {
+            path: 'mountains',
+            lazy: () => import('../features/manuscripts/MountainsPage'),
+          },
+        ],
+      },
+      {
+        path: 'favorites',
+        lazy: () => import('../features/favorites/FavoritesPage'),
+      },
+      {
+        path: 'recycle',
+        lazy: () => import('../features/recycle/RecyclePage'),
+      },
+      {
+        path: 'backup',
+        lazy: () => import('../features/backup/BackupPage'),
+      },
+      {
+        path: 'profile',
+        lazy: () => import('../features/profile/ProfilePage'),
+      },
+    ],
   },
   {
     path: '/canvas/:id',
